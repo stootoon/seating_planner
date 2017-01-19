fd659ef0a109b3647b6f88e2c045f2b5e597ff95
Sina Tootoonian <sina.tootoonian@gmail.com>
2017-01-19 12:53:56 +0200

    Incorporated table occupancy variance into the objective.

--- seating_plan.py
+++ seating_plan.py
@@ -3,7 +3,7 @@ from numpy import *
 import cvxpy as cvx
 from tqdm import tqdm
 
-verbose = True 
+verbose = True
 
 schedule_name = ""
 
@@ -20,10 +20,13 @@ availabilities = [] # A num_tables x num_dates binary matrix of table availabili
 preferences = []    # A num_guests x num_tables matrix of guest preferences for tables
 
 ## PARAMETERS
-max_guests_per_table_per_date = None 
-min_guests_per_table_per_date = None
-max_each_table_per_guest = None # The maximum number of dates each guest should sit at each table
+max_guests_per_table_per_date    = None 
+min_guests_per_table_per_date    = None
+max_each_table_per_guest         = None # The maximum number of dates each guest should sit at each table
 min_each_desired_table_per_guest = None # Minimum number of dates a guest should sit at a table they're interested in
+occupancy_variance_weight        = None # Weighting of occupancy variance in the loss function. 
+random_seed                      = None # Random seed
+num_rounding_iters               = None # Number of randomnly generated rounded schedules
 
 ## CONSTRAINTS MATRICES
 ## The rows in each of these matrices when applied to a potential schedule vector x
@@ -52,10 +55,17 @@ b_eq = [] # The values of the equalities
 A_ub = [] # The matrix specifying the elements involved in the upper bounds
 b_ub = [] # The values of the upper bounds
 
+### Table occupancy variance variables
+number_of_tables_per_date     = []
+mean_table_occupancy_per_date = []
+A_table_occupancy             = []
+
 ## LINEAR PROGRAMMING RESULTS
-relaxed_schedule = [] # What the linear program will return
-rounded_schedule = [] # The schedule after rounding
+relaxed_schedule   = [] # What the linear program will return
+rounded_schedule   = [] # The schedule after rounding
+schedule           = [] # The final schedule 
 schedule_as_matrix = [] # The final schedule, reshaped to a guests x (dates x tables) matrix.
+
 
 def LOG(string):
     if verbose:
@@ -73,16 +83,22 @@ def parse_input_file(input_file):
     global min_guests_per_table_per_date
     global max_each_table_per_guest
     global min_each_desired_table_per_guest
+    global occupancy_variance_weight
+    global random_seed
+    global num_rounding_iters
 
     parts = os.path.basename(input_file).split(".")
     schedule_name = parts[0]
-    LOG("Schedule name: {}".format(schedule_name))
+    LOG("SCHEDULE NAME: {}\n".format(schedule_name))
     
     state = "SCAN"
     with open(input_file,"r") as fp:
         for line in fp:
             line = line.strip()
             if len(line)==0:
+                continue
+
+            if line[0] == "#": # Comment line
                 continue
     
             old_state = copy(state)
@@ -161,6 +177,9 @@ def parse_input_file(input_file):
             
             if old_state != state:
                 LOG("'{}' caused {} => {}".format(line, old_state, state))
+                if state == "SCAN": # Insert a line break if we're back to scanning mode.
+                    LOG("")
+
 
 def build_constraints_matrices():
     ### Fills in the values of the constraints vectors and matrices.
@@ -181,6 +200,10 @@ def build_constraints_matrices():
     global b_guests__available_tables
     global b_dates__available_tables
     global b_dates__interested_guests_available_tables
+
+    ## Table occupancy variance variables
+    global mean_table_occupancy_per_date
+    global A_table_occupancy
     
     a0 = zeros((num_guests, num_dates, num_tables))
     
@@ -201,16 +224,27 @@ def build_constraints_matrices():
             a_tables__unavailable_tables[g,d,:] = reshape(a,(schedule_length,),order="C")
     A_tables__unavailable_tables = reshape(a_tables__unavailable_tables,(num_guests*num_dates, schedule_length), order="C")
     b_tables__unavailable_tables = A_tables__unavailable_tables[:,0]*0 + 1
-    
-    a_guests__available_tables = zeros((num_tables, num_dates, schedule_length))
+
+    # Keep a_guests__available_tables as num_dates x num_tables x schedule_length
+    # The table variance calculations assume it's in this order.  
+    a_guests__available_tables = zeros((num_dates, num_tables, schedule_length))
     for t in range(num_tables):
         for d in range(num_dates):
             a = copy(a0)
             a[:,d,t] = availabilities[t,d]
-            a_guests__available_tables[t,d,:] = reshape(a,(schedule_length,),order="C")
+            a_guests__available_tables[d,t,:] = reshape(a,(schedule_length,),order="C")
     A_guests__available_tables = reshape(a_guests__available_tables,(num_tables*num_dates, schedule_length), order="C")
     b_guests__available_tables = A_guests__available_tables[:,0]*0 + 1
-    
+
+    available_tables_per_date = sum(availabilities,axis=0).astype(int)
+    number_of_tables_per_date = zeros((sum(available_tables_per_date),))
+    ind = 0
+    for i in range(len(available_tables_per_date)):
+        number_of_tables_per_date[range(ind,ind+available_tables_per_date[i])] = available_tables_per_date[i]
+        ind = ind + available_tables_per_date[i]
+    mean_table_occupancy_per_date = len(guests)/number_of_tables_per_date.astype(float)
+    A_table_occupancy = remove_empty_rows(A_guests__available_tables,A_guests__available_tables[:,0])[0]
+
     a_dates__available_tables = zeros((num_guests, num_tables, schedule_length))
     for g in range(num_guests):
         for t in range(num_tables):
@@ -289,55 +323,65 @@ def build_linear_program():
     b_ub = concatenate((b_ub1, b_ub2, b_ub3, b_ub4), axis=0)
     A_ub, b_ub = remove_empty_rows(A_ub, b_ub)
 
-    LOG("Number of variables: {}".format(len(c)))
-    LOG("Number of upper-bound constraints: {}".format(A_ub.shape[0]))
-    LOG("Number of equality constraints: {}".format(A_eq.shape[0]))
-
-def run_linear_program():
-    global relaxed_schedule
+    LOG("\tNumber of variables: {}".format(len(c)))
+    LOG("\tNumber of upper-bound constraints: {}".format(A_ub.shape[0]))
+    LOG("\tNumber of equality constraints: {}".format(A_eq.shape[0]))
+    
+def run_convex_program():
     x = cvx.Variable(schedule_length)
-    objective = cvx.Minimize(-c.T * x)
+    objective = cvx.Minimize(-c.T * x + 0.5*occupancy_variance_weight*cvx.sum_squares(A_table_occupancy*x - mean_table_occupancy_per_date)/A_table_occupancy.shape[0])
     constraints  = [x>=0,
                     x<=1,
                     A_eq*x == b_eq,
                     A_ub*x <= b_ub]
     prob = cvx.Problem(objective, constraints)
     prob.solve()
-    print "status: {}".format(prob.status)
-    print "optimal value: {:.2f}".format(prob.value)
+    print "\tstatus: {}".format(prob.status)
+    print "\toptimal relaxed objective: {:.2f}".format(prob.value)
     if prob.value is Inf:
         raise Exception("Problem is infeasable. Stopping.")
     relaxed_schedule = reshape(array(x.value), (schedule_length,))
+    return relaxed_schedule
 
-def round_linear_programming_solution():
+def round_solution(relaxed_solution, num_rounds = 100, mode = "random"):
     ### The linear programming solution returns
     ### values in [0,1] for the seating assignments.
     ### We generate a binary schedule by treating these as probabilities
     ### and randomly assigning guests to tables according to their
     ### probability vectors over tables on a given date.
-    global rounded_schedule
-    rounded_schedule = 0*relaxed_schedule
-    for i in range(0, schedule_length, num_tables):
-        p = relaxed_schedule[range(i,i+num_tables)]
-        p[p<=0] = 1e-12 # Set to small non-zero value so we can safely sum.
-        p = p/sum(p)
-        which_table = random.choice(num_tables, p = p)
-        rounded_schedule[i + which_table] = 1
+    num_rounds    = {"random":num_rounds, "max":1}[mode]
+    best_schedule = copy(relaxed_solution)
+    best_loss     = Inf
+    losses        = zeros((num_rounds,))
 
-def generate_rounded_schedule(num_rounds = 100):
-    ### Runs the rounding a fixed number of times
-    ### and uses the value that agreed best with the guests'
-    ### preferences.
-    loss_values = zeros((num_rounds,))
-    for i in tqdm(range(num_rounds)):
-        random.seed(i)
-        round_linear_programming_solution()
-        loss_values[i] = dot(-c,rounded_schedule)
-    LOG("Loss values distribution after {} roundings: {:.2f} +/- {:.2f}".format(num_solutions, mean(loss_values), std(loss_values)))
-    ind_best = argmin(loss_values)
-    LOG("Best solution: {:.2f}".format(min(loss_values)))
-    random.seed(ind_best)
-    round_linear_programming_solution()
+    for r in tqdm(range(num_rounds)):
+        schedule = 0*relaxed_solution
+        for i in range(0, schedule_length, num_tables):
+            p = relaxed_schedule[range(i,i+num_tables)]
+            p[p<=0] = 1e-12 # Set to small non-zero value so we can safely sum.
+            p = p/sum(p)
+            which_table = {"random":random.choice(num_tables, p = p), "max":argmax(p)}[mode]
+            schedule[i + which_table] = 1
+
+        losses[r] = loss_function(schedule)
+
+        if losses[r] < best_loss:
+            best_loss = losses[r]
+            best_schedule = copy(schedule)
+    
+    return best_schedule, losses
+
+def config_from_ind(i):
+    table = mod(i, num_tables)
+    date  = mod((i - table)/num_tables, num_dates)
+    guest = (i - table - num_tables*date)/num_tables/num_dates
+    return (table, date, guest)
+
+def ind_from_config(table, date, guest):
+    return table + date*num_tables + guest*num_tables*num_dates
+
+def loss_function(s):
+    return -dot(c.T, s) + occupancy_variance_weight*0.5*mean((dot(A_table_occupancy,s) - mean_table_occupancy_per_date)**2)
 
 def write_schedule_as_list():
     file_name = "{}.schedule.list.txt".format(schedule_name)
@@ -358,7 +402,6 @@ def write_schedule_as_list():
                     fp.write("\t\t{:16s} ({})\n".format(guests_list[i], scores_list[i]))
 
 
-
 parse_input_file("week2.txt")
 num_tables = len(tables)
 num_guests = len(guests)
@@ -366,15 +409,18 @@ num_dates  = len(dates)
 
 schedule_length = num_tables*num_guests*num_dates
 
-LOG("BUILDING CONSTRAITS MATRICES.")
+random.seed(random_seed)
+
+LOG("BUILDING CONSTRAINTS MATRICES.")
 build_constraints_matrices()
-LOG("BUILDING LINEAR PROGRAM.")
+LOG("BUILDING CONVEX PROGRAM.")
 build_linear_program()
-LOG("RUNNING LINEAR PROGRAM.")
-run_linear_program()
+LOG("RUNNING CONVEX PROGRAM.")
+relaxed_schedule = run_convex_program()
 LOG("ROUNDING SOLUTION.")
-generate_rounded_schedule(num_rounds = 200)
-schedule_as_matrix = reshape(rounded_schedule, (num_guests, num_tables*num_dates), order="C")
+schedule, losses = round_solution(relaxed_schedule, num_rounds = num_rounding_iters, mode="random")
+LOG("Objective for final schedule: {}".format(loss_function(schedule)))
+schedule_as_matrix = reshape(schedule, (num_guests, num_tables*num_dates), order="C")
 LOG("WRITING SCHEDULE AS LIST.")
 write_schedule_as_list()
 LOG("DONE.")
